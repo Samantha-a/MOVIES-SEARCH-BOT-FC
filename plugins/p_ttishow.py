21a390cf6eaba243d3e44f8eae4bf700c35581fa
Samantha-a <95078987+Samantha-a@users.noreply.github.com>
2022-01-27 19:52:52 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
-from info import ADMINS,SUPPORT_CHAT
+from info import ADMINS
 from database.users_chats_db import db
 from database.ia_filterdb import Media
 from utils import get_size, temp
